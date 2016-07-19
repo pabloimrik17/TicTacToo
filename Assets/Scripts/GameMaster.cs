3b6ef9394c085db8061526d45d1b0a1a61750fc1
Pablo <pabloimrik17@gmail.com>
2016-07-19 20:55:58 +0200

    Logo, Multilanguage system

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour {
+
+  private static GameMaster instance;
+  public TextAsset stringFile;
+
+  public static GameMaster Instance {
+        get {
+            if (instance == null) {
+                instance = new GameMaster ();
+            }
+
+            return instance;
+        }
+    }
+	// Use this for initialization
+	void Start () {
+        Translator a = new Translator(stringFile);
+        string[] texto = a.GetText("uniqueID", "en");
+        print(texto[0]);
+        print(SceneManager.GetActiveScene().name);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
