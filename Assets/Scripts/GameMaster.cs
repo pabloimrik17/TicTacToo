0dfe099a525f3ffdcd3276019bdcaffbcc75160c
Pablo <pabloimrik17@gmail.com>
2016-07-20 21:02:13 +0200

    Menu Principal listo.
    Traduccion de UI funcionando

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -8,20 +8,30 @@ public class GameMaster : MonoBehaviour {
   public TextAsset stringFile;
 
   public static GameMaster Instance {
-        get {
-            if (instance == null) {
-                instance = new GameMaster ();
-            }
+    get {
+      if (instance == null) {
+        instance = new GameMaster();
+      }
+      return instance;
+    }
+  }
+    
 
-            return instance;
+    void Awake () {
+        if (instance != null) {
+            Destroy (gameObject);
+        } else {
+            instance = this;
+            GameObject.DontDestroyOnLoad (gameObject);
         }
     }
+
 	// Use this for initialization
 	void Start () {
-        Translator a = new Translator(stringFile);
-        string[] texto = a.GetText("uniqueID", "en");
+        string[] texto = Translator.Instance.GetText("StartJugarIA", "es");
         print(texto[0]);
         print(SceneManager.GetActiveScene().name);
+        TraduceUI();
 
 	}
 	
@@ -29,4 +39,23 @@ public class GameMaster : MonoBehaviour {
 	void Update () {
 	
 	}
+
+  void OnLevelWasLoaded(int nivel) {
+    print("llego");
+    if (nivel == 1) {
+        TraduceUI();
+     print("hola");
+    }
+  }
+
+  public void TraduceUI() {
+        GameObject[] arrUI = GameObject.FindGameObjectsWithTag("UITraducible");
+        string valor = "";
+        foreach(GameObject ui in arrUI) {
+            valor = Translator.Instance.GetText(ui.name, "es")[0];
+            if(valor != "") {
+                ui.GetComponent<UnityEngine.UI.Text>().text = valor;
+            }
+        }    
+    }
 }
